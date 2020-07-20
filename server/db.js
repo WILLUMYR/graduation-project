ad1e818b695191d3f178f136fc682000e8ddcb46
Alina Bylkova <bylkova11@gmail.com>
2020-07-21 00:10:45 +0200

    Restructured database

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const url = process.env.MONGO_URI;
+console.log('--------', url);
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -14,34 +15,58 @@ db.once('open', () => {
   console.log('MongoDB successfully connected!');
 });
 
+// Create a schema for patients
 const patientSchema = new Schema({
   username: { type: String, required: true },
-  userPassword: { type: String, required: true },
-  userEmail: { type: String, required: false },
-  case: {
-    caseDescription: { type: Object, required: false },
-    caseTaken: { type: Boolean, required: true },
-    healthProfessional: { type: Number, required: true },
-    messages: [
-      {
-        message: { type: String },
-        isPatient: { type: Boolean },
-      },
-    ],
-  },
+  password: { type: String, required: true },
+  email: { type: String, required: false },
+  gender: { type: String, enum: ['male', 'female', 'none'] },
+  created: { type: Date, default: Date.now },
+  lastLogin: { type: Date },
+  cases: [{ type: Schema.Types.ObjectId, ref: 'cases' }],
+});
+
+// Create a schema for psychologists
+const psychologistSchema = new Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  created: { type: Date, default: Date.now },
+  lastLogin: { type: Date },
+  workingStatus: { type: String, enum: ['active', 'inactive', 'onVacation', 'deactivated'] },
+  cases: [{ type: Schema.Types.ObjectId, ref: 'cases' }],
+});
+
+// Create a schema for cases
+const casesSchema = new Schema({
+  patientId: { type: Schema.Types.ObjectId, ref: 'patients', required: true },
+  psychologistId: { type: Schema.Types.ObjectId, ref: 'users' },
+  activeStatus: { type: Boolean, default: false },
+  description: { type: String, required: false },
+  created: { type: Date, default: Date.now },
+  closed: { type: Boolean, default: false },
+  messages: [
+    {
+      text: { type: String, required: true },
+      respondent: { type: String, enum: ['patient', 'psychologist'] },
+      respondentId: { type: Schema.Types.ObjectId },
+      respondentName: { type: String, required: true },
+      created: { type: Date, default: Date.now },
+    },
+  ],
+  notes: [
+    {
+      text: { type: String, required: true },
+      created: { type: Date, default: Date.now },
+    },
+  ],
 });
 
 const Patients = mongoose.model('patients', patientSchema);
 
-const healthProSchema = new Schema({
-  name: { type: String, required: true },
-  profession: { type: String, required: true },
-  email: { type: String, required: true },
-  userPassword: { type: String, required: true },
-  cases: [String],
-});
+const Psychologists = mongoose.model('psychologists', psychologistSchema);
 
-const healthPro = mongoose.model('healthPro', healthProSchema);
+const Cases = mongoose.model('cases', casesSchema);
 
 const newPatient = new Patients({
   username: 'user name',
@@ -62,20 +87,6 @@ const newPatient = new Patients({
     ],
   },
 });
-
-const casesSchema = new Schema({
-  patientId: { type: String, required: true },
-  healthProId: { type: String, required: false },
-  description: { type: String, required: false },
-  created: Date.now(),
-  closed: { type: Boolean, required: true },
-  messsages: [{
-    message: { type: String, required: true },
-    isPatient: { type: Boolean, required: true },
-  }],
-});
-
-const Cases = mongoose.model('cases', casesSchema);
 
 const newCase = new Cases({
   something: 'hello',
