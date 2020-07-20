cf1716977baa4448d03ae24f1dd1484575493073
Alina Bylkova <bylkova11@gmail.com>
2020-07-20 16:17:59 +0200

    Created endpoints for API

--- server/index.js
+++ server/index.js
@@ -8,4 +8,16 @@ app.use(bodyParser());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.post('/api/patients', (req, res) => {});
+
+app.post('/api/employees', (req, res) => {});
+
+app.post('/api/patients/', (req, res) => {});
+
+app.post('/api/cases', (req, res) => {});
+
+app.post('/api/cases/:id/messages', (req, res) => {});
+
+app.delete('/api/cases/:id', (req, res) => {});
+
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
