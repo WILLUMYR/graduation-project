dc70b454bd06899646f51228e42574fadb889b5a
Ymir <36711026+Ymirke@users.noreply.github.com>
2020-07-21 14:28:13 +0200

    creating handlesubmit for signup form.

--- client/src/components/PatientLogin.tsx
+++ client/src/components/PatientLogin.tsx
@@ -1,28 +1,46 @@
 import React, { useState } from 'react';
 
 export default function PatientLogin() {
-
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [gender, setGender] = useState('');
 
+  const handleSubmit = (event: any) => { // maybe not any?
+    event.preventDefault();
+    const obj: Object = {
+      username,
+      password,
+      email,
+      gender
+    }
+    fetch('/api/patients', {
+      method: 'POST',
+      body: obj,
+    })
+  }
 
   return (
-    <form action="/api/patients" method="post">
+    <form onSubmit={handleSubmit}>
       <input placeholder="Username" type="text" onChange={event => {
         setUsername(event.target.value);
       }} />
       <br />
-      <input placeholder="Password" type="password" />
+      <input placeholder="Password" type="password" onChange={event => {
+        setPassword(event.target.value);
+      }} />
       <br />
-      <select name="Select Gender" id="">
+      <select name="Select Gender" id="" onChange={event => {
+        setGender(event.target.value);
+      }}>
         <option value="none">Gender: neither / rather not say</option>
         <option value="male">male</option>
         <option value="female">female</option>
       </select>
       <br />
-      <input placeholder="Email (optional)" type="text" />
+      <input placeholder="Email (optional)" type="text" onChange={event => {
+        setEmail(event.target.value);
+      }} />
       <input type="submit" value="Submit" />
     </form>
   );
