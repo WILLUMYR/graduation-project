ea94d10102787d4b7aae9f5635f64134e4bf45ce
Alina Bylkova <bylkova11@gmail.com>
2020-07-21 10:40:29 +0200

    Exported schemas to api file

--- server/api/api.js
+++ server/api/api.js
@@ -1,4 +1,19 @@
+require('dotenv').config({ path: '../.env' });
 const router = require('express').Router();
+const mongoose = require('mongoose');
+const { Patients, Psychologists, Cases } = require('../models/models');
+
+const url = process.env.MONGO_URI;
+
+console.log(url);
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('MongoDB successfully connected!');
+});
 
 router.get('/', (req, res) => res.send('Hello World!'));
 
@@ -12,24 +27,21 @@ router.post('/patients', (req, res) => {
       created: undefined,
       lastLogin: undefined,
       cases: [],
-    }
-  }
-  // lag patient
-  // post patient til db
-  // gi response til bruker
+    };
+  };
 });
 
-router.post('/employees', (req, res) => { });
+router.post('/employees', (req, res) => {});
 
-router.post('/cases', (req, res) => { });
+router.post('/cases', (req, res) => {});
 
 router.get('/cases/:id', (req, res) => res.send('test123'));
 
-router.post('/cases/:id', (req, res) => { });
+router.post('/cases/:id', (req, res) => {});
 
-router.delete('/cases/:id', (req, res) => { });
+router.delete('/cases/:id', (req, res) => {});
 
-router.delete('/patients/:id', (req, res) => { });
+router.delete('/patients/:id', (req, res) => {});
 
 router.get('/api/cases', (req, res) => {
   const unassigned = req.params.unassigned;
--- server/models/models.js
+++ server/models/models.js
@@ -1,62 +1,64 @@
-require('dotenv').config({ path: '../.env' });
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const url = process.env.MONGO_URI;
-console.log('--------', url);
+const patientSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: false },
+    gender: { type: String, enum: ['male', 'female', 'none'] },
+    created: { type: Date, default: Date.now },
+    lastLogin: { type: Date },
+    cases: [{ type: Schema.Types.ObjectId, ref: 'cases' }],
+  },
+  {
+    timestamps: true,
+  },
+);
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const psychologistSchema = new Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    created: { type: Date, default: Date.now },
+    lastLogin: { type: Date },
+    workingStatus: { type: String, enum: ['active', 'inactive', 'onVacation', 'deactivated'] },
+    cases: [{ type: Schema.Types.ObjectId, ref: 'cases' }],
+  },
+  {
+    timestamps: true,
+  },
+);
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('MongoDB successfully connected!');
-});
-
-const patientSchema = new Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  email: { type: String, required: false },
-  gender: { type: String, enum: ['male', 'female', 'none'] },
-  created: { type: Date, default: Date.now },
-  lastLogin: { type: Date },
-  cases: [{ type: Schema.Types.ObjectId, ref: 'cases' }],
-});
-
-const psychologistSchema = new Schema({
-  fullName: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  created: { type: Date, default: Date.now },
-  lastLogin: { type: Date },
-  workingStatus: { type: String, enum: ['active', 'inactive', 'onVacation', 'deactivated'] },
-  cases: [{ type: Schema.Types.ObjectId, ref: 'cases' }],
-});
-
-const casesSchema = new Schema({
-  patientId: { type: Schema.Types.ObjectId, ref: 'patients', required: true },
-  psychologistId: { type: Schema.Types.ObjectId, ref: 'users' },
-  activeStatus: { type: Boolean, default: false },
-  description: { type: String, required: false },
-  created: { type: Date, default: Date.now },
-  closed: { type: Boolean, default: false },
-  messages: [
-    {
-      text: { type: String, required: true },
-      respondent: { type: String, enum: ['patient', 'psychologist'] },
-      respondentId: { type: Schema.Types.ObjectId },
-      respondentName: { type: String, required: true },
-      created: { type: Date, default: Date.now },
-    },
-  ],
-  notes: [
-    {
-      text: { type: String, required: true },
-      created: { type: Date, default: Date.now },
-    },
-  ],
-});
+const casesSchema = new Schema(
+  {
+    patientId: { type: Schema.Types.ObjectId, ref: 'patients', required: true },
+    psychologistId: { type: Schema.Types.ObjectId, ref: 'users' },
+    activeStatus: { type: Boolean, default: false },
+    description: { type: String, required: false },
+    created: { type: Date, default: Date.now },
+    closed: { type: Boolean, default: false },
+    messages: [
+      {
+        text: { type: String, required: true },
+        respondent: { type: String, enum: ['patient', 'psychologist'] },
+        respondentId: { type: Schema.Types.ObjectId },
+        respondentName: { type: String, required: true },
+        created: { type: Date, default: Date.now },
+      },
+    ],
+    notes: [
+      {
+        text: { type: String, required: true },
+        created: { type: Date, default: Date.now },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const Patients = mongoose.model('patients', patientSchema);
 
@@ -84,33 +86,22 @@ const newPatient = new Patients({
   },
 });
 
-const casesSchema = new Schema({
-  patientId: { type: String, required: true },
-  healthProId: { type: String, required: false },
-  description: { type: String, required: false },
-  closed: { type: Boolean, required: true },
-  messsages: [{
-    message: { type: String, required: true },
-    isPatient: { type: Boolean, required: true },
-  }],
-}, {
-  timestamps: true,
-});
-
-const Cases = mongoose.model('cases', casesSchema);
-
 const newCase = new Cases({
   patientId: '123',
   healthProId: null,
   description: 'problem...',
   closed: false,
-  messsages: [{
-    message: 'Thanks for reaching out, one of our health professionals will get to you as soon as possible.',
-    isPatient: false,
-  }],
+  messsages: [
+    {
+      message: 'Thanks for reaching out, one of our health professionals will get to you as soon as possible.',
+      isPatient: false,
+    },
+  ],
 });
 
 // newCase.save((err, newCase) => {
 //   if (err) return console.error(err);
 //   console.log(newCase.id);
 // });
+
+module.exports = { Patients, Psychologists, Cases };
