fc10d5973e9f9f242ffa75dda2abba5b13baad82
Markus W. Myrland <markusmyrland@hotmail.com>
2020-07-27 12:17:47 +0200

    Added switch case to dashboard

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 export default function Dashboard(props: any) {
   const [cases, setCases] = useState();
+  const [view, setView] = useState('YourCases');
 
   const getCases = () => {
     fetch('/api/cases', {
@@ -20,15 +21,46 @@ export default function Dashboard(props: any) {
     getCases();
   }, [])
 
-  if (cases !== undefined) {
-    return (
-      <section>
-        {cases.map((item: any) => {
-          return <p key={Math.random()}>{item.issue}</p>
-        })}
-      </section>
-    )
-  } else {
-    return <p>loading...</p>
+
+  const switchCase = (view: String) => {
+    switch (view) {
+      case 'YourCases':
+        return (
+          <p>Your cases</p>
+        )
+      case 'Unassigned':
+        return (
+          <p>Unassigned Cases</p>
+        )
+      case 'AllCases':
+        return (
+          <p>All Cases</p>
+        )
+    }
   }
+
+  return (
+    <>
+      <div>
+        <button className="dashboard__button" onClick={() => { setView('YourCases') }}>Your Cases</button>
+        <button className="dashboard__button" onClick={() => { setView('Unassigned') }}>Unassigned Cases</button>
+        <button className="dashboard__button" onClick={() => { setView('AllCases') }}>All Cases</button>
+      </div>
+      <section>
+        {switchCase(view)}
+      </section>
+    </>
+  )
+
+  // if (cases !== undefined) {
+  //   return (
+  //     <section>
+  //       {cases.map((item: any) => {
+  //         return <p key={Math.random()}>{item.issue}</p>
+  //       })}
+  //     </section>
+  //   )
+  // } else {
+  //   return <p>loading...</p>
+  // }
 }
