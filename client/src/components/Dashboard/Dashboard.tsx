201c1b4238c52968cd559b0a2f694f200f8af43d
Markus W. Myrland <markusmyrland@hotmail.com>
2020-07-28 10:41:45 +0200

    Adding and removing components endlessly

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -13,33 +13,51 @@ export default function Dashboard(props: any) {
     setSidebar(newCase[0]);
   };
 
-  const switchCase = (view: String) => {
-    switch (view) {
-      case 'YourCases':
-        return (
-          <CaseList url="api/cases/assigned" />
-        );
-      case 'Unassigned':
-        return (
-          <>
-            <div>
-              {getCases('api/cases/assigned')}
-              {getCases('api/cases/unassigned')}
-              {getCases('api/cases')}
-            </div>
-          </>
-        );
-      case 'AllCases':
-        return (
-          <>
-            <div>
-            </div>
-          </>
-        );
-      default:
-        return <p>Default</p>;
-    }
+  const getCases = (url: any) => {
+    fetch(url, {
+      headers: {
+        'x-auth-token': props.token,
+      },
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setCases(data);
+        // data.map((item: { createdAt: Date; _id: String; issue: React.ReactNode }) => {
+        //   return <CaseCard selectCase={selectCase} key={Math.random()} item={item} />;
+        // });
+      })
+      .catch(err => console.error(err));
   };
+
+  // const switchCase = (view: String) => {
+  //   switch (view) {
+  //     case 'YourCases':
+  //       return (
+  //         <CaseList url="api/cases/assigned" selectCase={selectCase} />
+  //       );
+  //     case 'Unassigned':
+  //       return (
+  //         <>
+  //           <div>
+  //             {getCases('api/cases/assigned')}
+  //             {getCases('api/cases/unassigned')}
+  //             {getCases('api/cases')}
+  //           </div>
+  //         </>
+  //       );
+  //     case 'AllCases':
+  //       return (
+  //         <>
+  //           <div>
+  //           </div>
+  //         </>
+  //       );
+  //     default:
+  //       return <p>Default</p>;
+  //   }
+  // };
 
   return (
     <>
